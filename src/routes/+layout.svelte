2156e77d11c6f438e9040999f4fe0122c784227f
Navya Nayer <navya.nayer03@gmail.com>
2024-09-22 10:34:39 +0530

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -145,7 +145,19 @@
   </main>
 </div>
 
-<script src="//code.tidio.co/nkkkm7zimewyrwp5n15kowbki7b5dwdc.js" async></script>
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/66efa3ef4cbc4814f7dd15b1/1i8c1go8u';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
 
 <footer>
   <p>&copy; 2024 Insurance Claims Platform. All rights reserved.</p>
